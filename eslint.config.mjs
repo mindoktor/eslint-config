895c93de9552a70c58009bc944d5322bae3b3267
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-11 15:52:33 +0200

    Run prettier on eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,12 @@
-import minDoktorEsLintConfig from './index.mjs'
+import minDoktorEsLintConfig from './index.mjs';
 
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-	{
-    ignores: ['**/node_modules/**/*'],
-		extends: [
-			minDoktorEsLintConfig,
-		],
-		rules: {
-      // Custom rules can be added here
-			// ...
-		},
-	}
-);
-
+export default tseslint.config({
+  ignores: ['**/node_modules/**/*'],
+  extends: [minDoktorEsLintConfig],
+  rules: {
+    // Custom rules can be added here
+    // ...
+  },
+});
