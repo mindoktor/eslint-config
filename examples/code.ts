86bc7e3cd7283b0808ef56c9fc83b6b00ea13416
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-11 15:54:41 +0200

    Remove underscore from example file

--- examples/code.ts
+++ examples/code.ts
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Example code snippet demonstrating ESLint configuration
 
 const c = 5;
 
-const _obj = {
+const obj = {
   a: 1,
   b: 2,
   c: c,
 };
 
-const _myFunction = (x: number) => {
+const myFunction = (x: number) => {
   return x * 2;
 };
 
@@ -16,19 +17,19 @@ const myAsyncFunction = async (x: number): Promise<number> => {
   return await Promise.resolve(x * 2);
 };
 
-const _funcUsingAwaitedAsync = async () => {
+const funcUsingAwaitedAsync = async () => {
   const _result = await myAsyncFunction(5);
 };
 
-const _funcUsingNonAwaitedAsync = () => {
-  const _promiseResult = myAsyncFunction(10);
+const funcUsingNonAwaitedAsync = () => {
+  const promiseResult = myAsyncFunction(10);
 };
 
-const _funcUsingNonAwaitedAsyncSideEffect = () => {
+const funcUsingNonAwaitedAsyncSideEffect = () => {
   void myAsyncFunction(15);
 };
 
 const meaningOfLife = 42;
 const isTrue = true;
 
-const _templateLiteral = `Example: ${meaningOfLife}-${isTrue}`;
+const templateLiteral = `Example: ${meaningOfLife}-${isTrue}`;
