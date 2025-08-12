4936551fa9515ec1091944ddff1a69454a7fc3e8
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-12 16:24:23 +0200

    Use globalIgnores to ensure not even trasversing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
 import minDoktorEsLintConfig from './src/index.mjs';
+import { globalIgnores } from 'eslint/config';
 
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  ignores: [
-    'node_modules/**/*',
-    'dist/*',
-    'dist/**/*',
-  ],
-  extends: [minDoktorEsLintConfig],
-  rules: {
-    // Custom rules can be added here
-    // ...
+export default tseslint.config(
+  {
+    extends: [minDoktorEsLintConfig],
+    rules: {
+      // Custom rules can be added here
+      // ...
+    },
   },
-});
+  globalIgnores([
+    'dist/', // ignore entire dist directory
+    'node_modules/',
+  ])
+);
