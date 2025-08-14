19519d89aa342d0827ffd99f3ebc9f70376b8e62
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-14 15:44:18 +0200

    Add prettier config inside stylistic definition

--- src/configs/stylistic.ts
+++ src/configs/stylistic.ts
@@ -2,6 +2,14 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 import tseslint from 'typescript-eslint';
 
+const prettierConfig = {
+  singleQuote: true,
+  parser: 'typescript',
+} as const;
+
 export const mindoktorStylistic = tseslint.config({
   extends: [eslintPluginPrettierRecommended],
+  rules: {
+    'prettier/prettier': ['error', prettierConfig],
+  },
 });
