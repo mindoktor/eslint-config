786d5cf1fa8ee3b56c56623b01b9bd4b28320527
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-18 17:57:42 +0200

    Add react-rules

--- src/configs/reactRecommended.ts
+++ src/configs/reactRecommended.ts
@@ -1,7 +1,12 @@
+import reactPlugin from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
 
 import { mindoktorRecommended } from './recommended.js';
 
 export const mindoktorReactRecommended = tseslint.config({
-  extends: [mindoktorRecommended],
+  extends: [
+    mindoktorRecommended,
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat['jsx-runtime'],
+  ],
 });
