6b344ab654501236b3cbaec0080feaf71207dd26
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-21 17:34:06 +0200

    Cleaner config

--- .release-it.ts
+++ .release-it.ts
@@ -5,20 +5,25 @@ const releaseItConfig: Config = {
     commitMessage: 'chore: bump version to ${version}',
     tagName: '${version}',
     requireCleanWorkingDir: true,
-    requireBranch: 'develop',
-    push: true,
+    push: false,
   },
   npm: {
     publish: false,
   },
   hooks: {
     'before:init': ['yarn lint', 'yarn typecheck'],
-    'before:bump': ['yarn cleanbuild'],
-    'after:bump': [
-      'git checkout -b ${version} && git push --set-upstream origin ${version}',
+    'before:bump': ['yarn cleanbuild', 'yarn add .'],
+    'before:release': [
+      'git checkout -b ${version}',
+      'git push --set-upstream origin ${version}',
     ],
     'after:release': [
-      'git checkout develop && git merge ${version} && git push && git checkout main && git merge develop && git push',
+      'git checkout develop',
+      'git merge ${version}',
+      'git push',
+      'git checkout main',
+      'git merge develop',
+      'git push',
     ],
   },
 };
