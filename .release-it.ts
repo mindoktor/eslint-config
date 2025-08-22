d919e9fcca003e8ac70075982180671c4021d7e6
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-22 17:52:53 +0200

    Simplify release script

--- .release-it.ts
+++ .release-it.ts
@@ -4,29 +4,24 @@ const releaseItConfig: Config = {
   git: {
     commit: true,
     commitMessage: 'chore: bump version to ${version}',
-    tagName: '${version}',
+    tag: false,
     requireCleanWorkingDir: true,
     requireBranch: 'develop',
     push: true,
-    pushArgs: '--set-upstream origin ${version}',
   },
   npm: {
     publish: false,
   },
   hooks: {
     'before:init': ['yarn lint', 'yarn typecheck'],
-    'before:release': [
+    // the "release" step only does the bump-commit on 'develop' and pushes to origin
+    'after:release': [
+      // the actual "release" happens here, by creating a new branch and pushing it with the built artifacts
       'git switch -C ${version}',
       'yarn cleanbuild',
       'git add dist -f',
-    ],
-    'after:release': [
-      'git checkout develop',
-      'git merge ${version}',
-      'git push',
-      'git checkout main',
-      'git merge develop',
-      'git push',
+      'git commit -m "chore: release ${version}"',
+      'git push --set-upstream origin ${version}',
     ],
   },
 };
