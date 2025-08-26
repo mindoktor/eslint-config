2b61ac621047c4c13c7b3565ba7cb640e984d0ec
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-26 11:43:49 +0200

    chore: release 1.0.3

---
+++ dist/eslint.config.js
@@ -0,0 +1,30 @@
+import { globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import minDoktorEsLintConfig from './src/index.js';
+export default tseslint.config({
+    extends: [minDoktorEsLintConfig],
+    // Language Options
+    languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parserOptions: {
+            projectService: true,
+        },
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+        },
+    },
+}, {
+    rules: {
+    // Custom rules can be added here
+    // ...
+    },
+}, globalIgnores([
+    'dist/', // ignore entire dist directory
+    'node_modules/',
+]));
+//# sourceMappingURL=eslint.config.js.map
