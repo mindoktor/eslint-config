5df97d052c1ab72279244a3ed60975dc38bf2b36
Fabio Colella <fabio.colella@mindoktor.se>
2025-08-27 19:26:51 +0200

    chore: release 1.1.0-1

---
+++ dist/src/index.d.ts
@@ -0,0 +1,7 @@
+export declare const configs: {
+    recommended: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+    reactRecommended: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+    stylistic: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+};
+declare const defaultConfig: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+export default defaultConfig;
