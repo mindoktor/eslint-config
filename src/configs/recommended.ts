8263a8e880b7cd5a2e47f99b411bb67d42cc4856
Fabio Colella (Min Doktor) <91458244+fabio-colella-md@users.noreply.github.com>
2025-08-28 11:18:29 +0200

    feat: Disable redundant import rules (#8)
    * Disable redundant import rules
    
    * chore: bump version to 1.1.0-0
    
    * Also disable troublesome no-deprecated
    
    * Sort disabled rules
    
    * chore: bump version to 1.1.0-1

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-named-as-default-member */
 import eslint from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
@@ -73,11 +72,20 @@ export const mindoktorRecommended = tseslint.config(
       'import/no-absolute-path': 'error',
       'import/no-amd': 'error',
       'import/no-cycle': 'warn',
-      'import/no-deprecated': 'warn',
       'import/no-extraneous-dependencies': 'error',
       'import/no-named-default': 'error',
       'import/no-self-import': 'error',
       'import/no-webpack-loader-syntax': 'error',
+      // Disable redundant rules
+      // See more:
+      // - Performance issues: https://typescript-eslint.io/troubleshooting/typed-linting/performance#eslint-plugin-import
+      // - Parse errors: https://github.com/typescript-eslint/typescript-eslint/issues/1333
+      'import/default': 'off',
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/no-deprecated': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
     },
   },
   {
